06e3d63a081473f210eb78a11560ad3aac10f9c8
feng42483-gif <feng42483@gmail.com>
2026-03-08 17:55:39 +0800

    Add files via upload

---
+++ 第三章/函数案例.py
@@ -0,0 +1,24 @@
+# # 案例1
+# def traingle_area(b,h):
+#     return b*h/2
+# print(traingle_area(20,30))
+
+#
+# # 案例2
+# def count_aeiou(s):
+#     count = 0
+#     for i in s:
+#         if i in "aeiouAEIOU":
+#             count += 1
+# #     return count
+# # print(count_aeiou("hello word,hello python"))
+#
+# # 案例3
+# def calc_score(score_list):
+#     max_s = max(score_list)
+#     min_s = min(score_list)
+#     avg_s= round(sum(score_list)/len(score_list),1)
+#     return max_s, min_s, avg_s
+# s_list = (599,611,455,566,786,312,465)
+# max_score, min_score, avg_score = calc_score(s_list)
+# print(max_score, min_score, avg_score)
