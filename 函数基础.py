7ff5f622838d2e9864618a40f806aff06e0c1eee
feng42483-gif <feng42483@gmail.com>
2026-03-08 17:54:25 +0800

    Add files via upload

---
+++ 函数基础.py
@@ -0,0 +1,39 @@
+# # # 圆的面积
+# def circle_area(r):
+#     area = 3.14 * r ** 2
+#     return area
+# area = circle_area(10)
+# print(area)
+# #
+# # # 长方形的面积
+# def rectangle_area(l,w):
+#     area = l * w
+#     return area
+# area = rectangle_area(20,10)
+# print(area)
+#
+# #计算圆的面积，周长
+# def circle_area_len(r):
+#     return 3.14 * r * r,round(2 * 3.14 * r, 1)
+# al = circle_area_len(10)
+# print(al)
+
+
+# 函数嵌套
+# def function_a():
+#     print("a")
+#     function_b()
+#     print("a")
+#
+# def function_b():
+#     print("b")
+#     function_c()
+#     print("b")
+#
+# def function_c():
+#     print("c")
+#
+# function_a()
+# print("1")
+
+#
