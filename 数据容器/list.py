529b0d3ed801c54b1a76eaf10ac442f988ecd6c1
feng42483-gif <feng42483@gmail.com>
2026-08-06 16:17:08 +0800

    Add files via upload

---
+++ 数据容器/list.py
@@ -0,0 +1,58 @@
+# num_list = []
+
+# for i in range(10):
+#     num = int(input("请输入数字："))
+#     num_list.append(num)
+# print(num_list)
+
+# num_list.sort()
+# print(num_list)
+
+# print(num_list[0])
+# print(num_list[-1])
+# print(sum(num_list)/len(num_list))
+
+
+
+# num_list =[111,121,56,32,24,80,90,22]
+# new_list=[i**2 for i in num_list if i % 2==0]
+# print(new_list)
+
+
+# s = [15,16,17,24,33,55,66,77,88]
+# s.append()在列表尾部追加元素
+# s.append(188)
+# print(s)
+
+# insert在指定索引之前插入元素
+# s.insert(1,189)
+# print(s)
+
+# remove 移除列表中第一个匹配的元素
+# s.remove(15)
+# print(s)
+
+# pop 删除列表中的指定索引元素之后并返回列表
+# e =s.pop(1)
+# print(s)
+# print(e)
+
+
+# 如若没有指定索引则默认删除最后一位
+# e = s.pop()
+# print(s)
+# print(e)
+
+
+# 排序
+# s.sort()
+# print(s)
+
+
+# 反转列表
+# s.reverse()
+# print(s)
+
+# 列表数平方
+# num_list = [ i**2 for i in s]
+# print(num_list)
