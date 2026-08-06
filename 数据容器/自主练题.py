2feda82ef0fca07ddc8d1804465b19f8a53836e0
feng42483-gif <feng42483@gmail.com>
2026-08-06 16:16:05 +0800

    Add files via upload

---
+++ 数据容器/自主练题.py
@@ -0,0 +1,21 @@
+# question_bank = [
+#     {"question": "5和6哪个大", "answer": "6", "difficulty": "简单"},
+#     {"question": "5和10哪个大", "answer": "10", "difficulty": "简单"},
+#     {"question": "15和10哪个大", "answer": "15", "difficulty": "简单"},
+#
+# ]
+# # 这里初始化
+# correct_count =0
+# wrong_count =0
+# for question in question_bank:
+#     print(question["question"])
+#     user_answer = input("你的答案是：")
+#     correct_answer = question["answer"]
+#     if user_answer == correct_answer:
+#         print("回答正确！")
+#         correct_count =   correct_count + 1
+#     else:
+#         print("回答错误！")
+#         wrong_count = wrong_count + 1
+#
+# print("正确：" + str(correct_count) + "题，错误：" + str(wrong_count) + "题")
