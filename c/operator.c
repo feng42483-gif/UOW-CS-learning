df22340f31dd888d2478fb0de5fee2725d6c37e4
feng42483-gif <feng42483@gmail.com>
2026-08-14 16:19:19 +0800

    Add files via upload

---
+++ c/operator.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	//printf("%d\n", 3 + 2);
+	//printf("%d\n", 3 - 2);
+	//printf("%d\n", 3 * 2);
+	////取余
+	//printf("%d\n", 10 % 2);
+
+
+	/*int ge = num % 10;
+	printf("个位数字为：%d\n", ge);
+	int shi = num / 10 % 10;
+	printf("十位上的数字为:%d\n", shi);
+	int bai = num / 100;
+	printf("百位上的数字为:%d\n", bai);*/
+
+
+
+	/*printf("请输入一个数字:\n");
+	int num ;
+	scanf("%d", &num);
+	printf("%d", num != 7 && num % 10 !=7 && num /10 % 10 != 7 && num %7 != 0  );*/
+
+	/*int a = 10;
+	int b = 20;
+	int c = 30;
+	int d = a > b ? a : b;
+	int max = b > c ? b : c;
+	printf("%d\n", max);*/
+
+
+	/*printf("请输入一个数字:\n");
+	int numa ;
+	scanf("%d", &numa);
+	int num = numa >= 0 ? numa : -numa;
+	printf("%d\n", num);
+	numa %= 3;
+	printf("%d\n", numa);
+	numa =  numa + 10;
+	printf("%d", numa);*/
+ 
+	/*int num = 2 * 3;
+	printf("%d", num);*/
+
+	//&& || !
+
+}
+
+
+	
+
+
+
+
+
+
+//+  - *
