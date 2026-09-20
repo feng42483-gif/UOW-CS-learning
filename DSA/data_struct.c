7984fecac1dfaf9d00ad34915ff236fceec3ddde
feng42483-gif <feng42483@gmail.com>
2026-09-20 18:31:52 +0800

    Add files via upload

---
+++ DSA/data_struct.c
@@ -0,0 +1,28 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<math.h>
+//#include<time.h>
+//#include<stdlib.h>
+//#include<string.h>
+
+//void printN(int N);
+//main()
+//{
+//	printN(100);
+//
+//}
+//
+//
+//void printN(int N)
+//{
+//	int i;
+//	for (i = 1; i < N; i++)
+//	{
+//		printf("%d\n", i);
+//	}
+//	return 0;
+//
+//}
+
+
+
